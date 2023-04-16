2e25c0ec5d6b653e7a7bc4af8fb8f4b4472c384c
mizunomu1v1 <fulltime.furotime@gmail.com>
2023-04-16 23:50:40 +0900

    3章終わりまで

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,8 +5,13 @@ import { HEROS } from '../mock-heros';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
   heroes = HEROS;
+  selectedHero?: Hero;
+  // 送られてきたheroをselectedHeroに格納
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
